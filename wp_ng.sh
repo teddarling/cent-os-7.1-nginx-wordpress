99e56497edd4484cb9ee8710df20e362e1088bbf
Ted Darling <ted@digitalspirittech.com>
2015-10-17 11:50:02 -0500

    add cache purge module for NGINX so that we have it for the build. We want to add it for purging the cache later.

--- wp_ng.sh
+++ wp_ng.sh
@@ -6,6 +6,10 @@ NGINX_VERSION="nginx-1.8.0"
 NGINX_FILE="$NGINX_VERSION.tar.gz"
 NGINX_URL="nginx.org/download/$NGINX_FILE"
 
+NGINX_CACHE_PURGE_VERSION="ngx_cache_purge-2.3"
+NGINX_CACHE_PURGE_FILE="$NGINX_CACHE_PURGE_VERSION.tar.gz"
+NGINX_CACHE_PURGE_URL="labs.frickle.com/files/$NGINX_CACHE_PURGE_FILE"
+
 
 # If the build directory doesn't exist, create it
 if [ ! -d "$BUILD_DIR" ]; then
@@ -27,17 +31,17 @@ if ! sudo yum list installed wget; then
   sudo yum -y install wget
 fi
 
+# Create the nginx user if it doesn't exist
+if ! id -u nginx >/dev/null 2>&1; then
+echo "Adding nginx user"
+useradd nginx
+usermod -s /sbin/nologin nginx
+fi
+
 # Download NGINX, if the file doesn't exist.
 if [ ! -f "$NGINX_FILE" ]; then
   echo "Downloading nginx from ($NGINX_URL)"
   wget "$NGINX_URL"
-fi
-
-# Create the nginx user if it doesn't exist
-if ! id -u nginx >/dev/null 2>&1; then
-  echo "Adding nginx user"
-  useradd nginx
-  usermod -s /sbin/nologin nginx
 fi
 
 # If the nginx directory for extraction exists, delete it.
@@ -49,6 +53,22 @@ fi
 # Extract the NGINX file.
 echo "Extracting NGINX"
 tar -xvzf $NGINX_FILE
+
+# Download NGINX cache purge, if the file doesn't exist.
+if [ ! -f "$NGINX_CACHE_PURGE_FILE" ]; then
+  echo "Downloading nginx from ($NGINX_CACHE_PURGE_FILE)"
+  wget "$NGINX_CACHE_PURGE_FILE"
+fi
+
+# If the nginx cache purge directory for extraction exists, delete it.
+if [ -d "$NGINX_CACHE_PURGE_VERSION" ]; then
+  echo "Removing NGINX cache purge directory"
+  sudo rm -rf "$NGINX_CACHE_PURGE_VERSION"
+fi
+
+# Extract the NGINX cache purge file.
+echo "Extracting NGINX cache purge"
+tar -xvzf $NGINX_CACHE_PURGE_FILE
 
 # move to the directory created by extracting the NGINX file.
 echo "Move to NGINX directory"
