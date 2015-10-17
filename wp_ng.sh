69af20a1c408fb512dcced37489cf07b7a7cc0b1
Ted Darling <ted@digitalspirittech.com>
2015-10-16 21:50:26 -0500

    create nginx user if it doesn't exist

--- wp_ng.sh
+++ wp_ng.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-BUILD_DIR = "$HOME/wp_ng"
+# Build directory that we are going to work with.
+BUILD_DIR="$HOME/wp_ng"
 
-echo $BUILD_DIR
+# If the build directory doesn't exist, create it
+if [ ! -d "$BUILD_DIR" ]; then
+  echo "Create build directory"
+  mkdir -pv $BUILD_DIR
+fi
 
-echo "Move to Home Folder"
-cd $HOME
-mkdir $BUILD_DIR
+# move to the build directory
+echo "Move to build directory"
 cd $BUILD_DIR
+
+# Download NGINX
+# wget nginx.org/download/nginx-1.8.0.tar.gz
+
+# Create the nginx user if it doesn't exist
+if ! id -u nginx >/dev/null 2>&1; then
+  echo "Adding nginx user"
+  useradd nginx
+  usermod -s /sbin/nologin nginx
+fi
