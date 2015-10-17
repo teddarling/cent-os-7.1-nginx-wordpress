ae947f4a89585585e1d44f21d4b23d4daec47e15
Ted Darling <ted@digitalspirittech.com>
2015-10-17 18:16:56 -0500

    auto answer yes on yum

--- install_php.sh
+++ install_php.sh
@@ -14,4 +14,4 @@ rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum -y update
 
 # Install PHP 7 FPM
-sudo yum --enablerepo=webtatic-testing install php70w php70w-opcache php70w-common php70w-bcmath php70w-cli php70w-fpm php70w-gd php70w-imap php70w-mbstring php70w-mcrypt php70w-mysqlnd php70w-odbc php70w-pgsql php70w-pspell php70w-soap php70w-tidy php70w-xml php70w-xmlrpc
+sudo yum -y --enablerepo=webtatic-testing install php70w php70w-opcache php70w-common php70w-bcmath php70w-cli php70w-fpm php70w-gd php70w-imap php70w-mbstring php70w-mcrypt php70w-mysqlnd php70w-odbc php70w-pgsql php70w-pspell php70w-soap php70w-tidy php70w-xml php70w-xmlrpc
