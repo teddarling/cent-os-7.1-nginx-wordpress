1fbf95d2589bb1bb4f61a29106240a742df16ee4
Ted Darling <ted@digitalspirittech.com>
2015-10-18 10:49:49 -0500

    change out the nginx.conf that is created by the build process with our own and restart nginx.

--- build_ng.sh
+++ build_ng.sh
@@ -164,6 +164,11 @@ sudo wget -O /etc/nginx/conf.d/default.conf https://raw.githubusercontent.com/te
 sudo cp /etc/nginx/html/index.html /usr/share/nginx/default/public_html
 
 # Copy the default nginx.conf file that we want to use. What comes with the build is horrible.
+sudo wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/teddarling/cent-os-7.1-nginx-wordpress/master/nginx.conf
+
+# Restart nginx to use the new conf files.
+sudo systemctl restart nginx
+
 
 # Delete the build directory to clean up after ourself.
 sudo rm -rf $build_dir
