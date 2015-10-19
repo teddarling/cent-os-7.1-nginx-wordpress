ba7b44ae0b078868188cadaa9bbf309aa309c1bf
Ted Darling <ted@digitalspirittech.com>
2015-10-19 16:16:37 -0500

    add expect script to answer questions for mysql_secure_installation

--- secure_maria_db.sh
+++ secure_maria_db.sh
@@ -51,4 +51,40 @@ fi
 echo 'Securing MariaDB'
 sudo yum -y install expect
 
+echo "Run mysql_secure_installation"
+
+SECURE_MYSQL=$(expect -c "
+
+set timeout 10
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+
+expect \"Change the root password?\"
+send \"y\r\"
+
+expect \"New password:\"
+send \"$maria_password\r\"
+
+expect \"Re-enter new password:\"
+send \"$maria_password\r\"
+
+expect \"Remove anonymous users?\"
+send \"y\r\"
+
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
 
