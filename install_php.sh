66de831542d2bf5a325ffd3bd628a2e3c25ee023
Ted Darling <ted@digitalspirittech.com>
2015-10-19 15:05:57 -0500

    add usage for instal_php.sh

--- install_php.sh
+++ install_php.sh
@@ -6,6 +6,46 @@
 #  Created by Theodore Darling on 10/17/15.
 #
 
+# Message to display if no valid arguments
+usage()
+{
+cat << EOF
+    usage: $0 options
+
+    This script will download the expected version of NGINX and NGINX_Cache_purge to the folder specified and build it with the correct user
+
+    OPTIONS:
+        -h      Show this message
+        -u      User to create for NGINX
+        ?       Show this message
+EOF
+}
+
+# Parse the arguments
+while getopts "hc:d:n:u:" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        u) # User to assign to the build
+            nginx_user="$OPTARG"
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+# If empty arguments given, show usage of script
+if [[ -z "$nginx_user" ]]
+then
+    usage
+    exit 1
+fi
+
 # Get the yum repos needed for PHP 7
 echo "Getting Repos"
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
