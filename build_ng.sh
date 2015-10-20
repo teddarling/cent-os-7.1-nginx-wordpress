6fe20bfec4bfcd9288c62b26bce473d770af3156
Ted Darling <ted@digitalspirittech.com>
2015-10-20 09:57:18 -0500

    Try moving the files again. Seem to be getting an error copying them.

--- build_ng.sh
+++ build_ng.sh
@@ -231,7 +231,9 @@ else
     echo "Downloading phpMyAdmin and moving to web folder"
     sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.5.0.2/phpMyAdmin-4.5.0.2-english.tar.gz
     sudo tar zxvf phpMyAdmin-4.5.0.2-english.tar.gz
-    sudo mv phpMyAdmin-4.5.0.2-english /usr/share/nginx/phpMyAdmin/public_html
+# sudo mv phpMyAdmin-4.5.0.2-english /usr/share/nginx/phpMyAdmin/public_html
+    sudo cp phpMyAdmin-4.5.0.2-english
+    sudo mv * /usr/share/nginx/phpMyAdmin/public_html
 
     if [ -z "$php_my_admin_port" ]; then
         echo "Set default port of 8181 for phpMyAdmin"
@@ -248,7 +250,7 @@ else
 
     echo "moving to subfolder of main site."
     sudo mkdir -pv /usr/share/nginx/default/public_html/phpmyadmin
-    sudo cp /usr/share/nginx/phpMyAdmin/public_html/phpMyAdmin-4.5.0.2-english /usr/share/nginx/default/public_html/phpmyadmin
+#sudo cp /usr/share/nginx/phpMyAdmin/public_html/phpMyAdmin-4.5.0.2-english /usr/share/nginx/default/public_html/phpmyadmin
     sudo chown -R nginx:nginx /usr/share/nginx/default/public_html/phpmyadmin
 
     # Restart nginx to add the new conf file.
