b594b6b09a7151ced7eae45e0c68e1c2253a4543
Ted Darling <ted@digitalspirittech.com>
2015-11-24 15:49:51 -0600

    now run the install on PHP and change some settings and user info.

--- php/install_php.sh
+++ php/install_php.sh
@@ -34,3 +34,26 @@ else
     echo "Webtatic Installed"
 fi
 
+# Install PHP-FPM if it isn't already installed.
+if ! which php-fpm > /dev/null 2>&1
+then
+    echo "Installing PHP-FPM"
+    sudo yum -y install php70w php70w-mysql php70w-fpm php70w-cli php70w-common php70w-bcmath php70w-gd php70w-imap php70w-mbstring php70w-mbstring php70w-opcache php70w-pear php70w-pgsql php70w-pspell php70w-soap php70w-tidy php70w-xml php70w-xmlrpc
+
+    # Secure PHP a little here.
+    sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php.ini
+
+    # Change user for PHP
+    sudo sed -i 's/user = apache/user = nginx/' /etc/php-fpm.d/www.conf
+    sudo sed -i 's/group = apache/group = nginx/' /etc/php-fpm.d/www.conf
+
+    # Change session permissions from apache to nginx
+    sudo chown nginx:nginx /var/lib/php/session
+
+    # Start and enable PHP-FPM
+    echo "Starting and enabling PHP-FPM"
+    sudo systemctl start php-fpm
+    sudo systemctl enable php-fpm
+else
+    echo "PHP-FPM installed"
+fi
