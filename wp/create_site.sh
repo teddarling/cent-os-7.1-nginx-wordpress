bfc00db026a770e828e5b7be644935472b4c6b41
Ted Darling <ted@digitalspirittech.com>
2015-11-27 13:15:06 -0600

    Setup a shared WordPress configuration for consistency.

--- wp/create_site.sh
+++ wp/create_site.sh
@@ -73,6 +73,12 @@ site_conf="/etc/nginx/conf.d/$wp_domain.conf"
 # Copy the config file for this domain into a conf file for NGINX
 sudo wget -O "$site_conf" https://raw.githubusercontent.com/teddarling/cent-os-7.1-nginx-wordpress/master/wp/site_template.conf
 
+# If shared WP config file doesn't exist, copy it to server.
+if [ ! -f /etc/nginx/wp.conf ]
+then
+    sudo wget -O /etc/nginx/wp.conf https://raw.githubusercontent.com/teddarling/cent-os-7.1-nginx-wordpress/master/wp/wp.conf
+fie
+
 # Change the name of the
 sudo sed -i 's/replace_server/'$wp_domain'/g' "$site_conf"
 
